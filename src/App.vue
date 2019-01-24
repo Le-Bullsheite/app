2966f8d745c30b029f94616e8299480ad2df7e2c
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-24 10:10:09 -0800

    hello world acheived

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  
+  <div>
+    <QuoteBlock/>
+  </div>
 </template>
 
 <script>
+import QuoteBlock from './components/QuoteBlock.vue';
 export default {
-
+  components: {
+    QuoteBlock
+  }
 }
 </script>
 
