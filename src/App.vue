be9210c5b8e485f1511b61698e10428228aafc49
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-25 10:14:01 -0800

    quoteblock component done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <QuoteBlock/>
+    <QuoteBlock
+    :quoteBlockName="quoteBlockName"
+    :quoteBlockDescription="quoteBlockDescription"
+    :quoteBlockText="quoteBlockText"
+    />
   </div>
 </template>
 
 <script>
 import QuoteBlock from './components/QuoteBlock.vue';
 export default {
+  data() {
+    return {
+      quoteBlockName: 'Kayne West',
+      quoteBlockDescription: 'Voice of a Generation',
+      quoteBlockText: 'I am Warhol. I am the number one most impactful artist of our generation. I am Shakespeare in the flesh.'
+    }
+  },
   components: {
     QuoteBlock
   }
+  
 }
 </script>
 
 <style>
-
+body {
+  margin: 0;
+}
 </style>
