8fb78acff950e6f26b7edda5ff51770d5e89746e
Marcy Silverman <marcysilve225@gmail.com>
2019-01-25 10:08:42 -0800

    added the router

---
+++ router.js
@@ -0,0 +1,15 @@
+import VueRouter from 'vue-router';
+import Home from './src/components/Home';
+import About from './src/components/About';
+import Careers from './src/components/Careers';
+import Contact from './src/components/Contact';
+
+export default new VueRouter ({
+    routes: [
+        { path : '/home', component: Home },
+        { path: '/about', component: About },
+        { path: '/careers', component: Careers },
+        { path: '/contact', component: Contact },
+        { path: '*', redirect: '/home' }
+    ]
+});
